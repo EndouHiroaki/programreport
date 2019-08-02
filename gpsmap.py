16eb8da342f695298c8e4262f5bb2e163dd57a5e
EndouHiroaki <53252050+EndouHiroaki@users.noreply.github.com>
2019-08-02 13:06:31 +0900

    Update gpsmap.py

--- gpsmap.py
+++ gpsmap.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
                 return val
         return ''
 
-img = Image.open("gpsimg.jpg")
+im = Image.open("gpsimg.jpg")
 datetime = get_datetime(img)
 print(datetime)
 
